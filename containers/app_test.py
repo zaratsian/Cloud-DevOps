ba49eb6ff97febfdae3cc376a523e47e3a5b9486
Dan Zaratsian <dzaratsian@google.com>
2019-04-05 18:00:03 -0400

    testing delta

--- containers/app_test.py
+++ containers/app_test.py
@@ -1,7 +1,7 @@
 import sys
 import datetime
 
-print(' [ INFO ] Sys Version: {}'.format(sys.version) )
-print( '[ INFO ] Current DateTime is: {}'.format( datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') ))
-
+print('[ INFO ] Sys Version: {}'.format(sys.version) )
+print('[ INFO ] Current DateTime is: {}'.format( datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') ))
+print('[ END ]')
 
